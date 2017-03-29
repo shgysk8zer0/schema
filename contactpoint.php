9f9e7ca81ac22e4d999627010a4fe473e868dd29
Chris Zuber <shgysk8zer0@gmail.com>
2017-03-29 14:05:36 -0700

    Add hours available

--- contactpoint.php
+++ contactpoint.php
@@ -5,4 +5,16 @@ namespace shgysk8zer0\Schema;
 class ContactPoint extends Thing
 {
 	const ITEMTYPE = 'ContactPoint';
+
+	final public function setHoursAvailable(OpeningHoursSpecification ...$hours)
+	{
+		foreach ($hours as $day) {
+			$this->addHoursAvailable($day);
+		}
+	}
+
+	final public function addHoursAvailable(OpeningHoursSpecification $hours)
+	{
+		$this->_add('hoursAvailable', $hours);
+	}
 }
