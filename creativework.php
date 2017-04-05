7e90ca793092f9b03bc49782d374eaf9133d2d96
Chris Zuber <shgysk8zer0@gmail.com>
2017-04-05 07:33:16 -0700

    Add more properties/methods to CreativeWork

--- creativework.php
+++ creativework.php
@@ -42,4 +42,134 @@ class CreativeWork extends Thing
 			));
 		}
 	}
+
+	final public function setCopyrightHolder(Thing $holder): self
+	{
+		if ($holder instanceof Person or $holder instanceof Organization) {
+			return $this->_set('copyrightHolder', $holder);
+		} else {
+			throw new \InvalidArgumentException(sprintf(
+				'copyrightHolder must be an instance of Person or Organization. %s given',
+				get_class($holder)
+			));
+		}
+	}
+
+	final public function setCopyrightYear(Int $year): self
+	{
+		return $this->_set('copyrightYear', $year);
+	}
+
+	final public function setDateCreated(\DateTime $created): self
+	{
+		return $this->_set('dateCreated', $created->format(\DateTime::ISO8601));
+	}
+
+	final public function setEditor(Person $editor): self
+	{
+		return $this->_set('editor', $editor);
+	}
+
+	final public function setGenre(String $genre): self
+	{
+		return $this->_set('genre', $genre);
+	}
+
+	final public function setHeadline(String $headline): self
+	{
+		return $this->_set('headline', $headline);
+	}
+
+	final public function setIsFamilyFriendly(Bool $fam_friendly): self
+	{
+		return $this->_set('isFamilyFriendly', $fam_friendly);
+	}
+
+	final public function setKeywords(String $keywords): self
+	{
+		return $this->_set('keywords', $keywords);
+	}
+
+	final public function setOffers(Offer ...$offers): self
+	{
+		foreach ($offers as $offer) {
+			$this->addOffers($offer);
+		}
+		return $this;
+	}
+
+	final public function addOffers(Offer $offer): self
+	{
+		return $this->_add('offers', $offer);
+	}
+
+	final public function setProducer(Thing $producer): self
+	{
+		if ($producer instanceof Person or $producer instanceof Organization) {
+			return $this->_set('producer', $producer);
+		} else {
+			throw new \InvalidArgumentException(sprintf(
+				'Producer must be an instance of Person or Organization. %s given',
+				get_class($producer)
+			));
+		}
+	}
+
+	final public function setPublisher(Thing $publisher): self
+	{
+		if ($publisher instanceof Person or $publisher instanceof Organization) {
+			return $this->_set('publisher', $publisher);
+		} else {
+			throw new \InvalidArgumentException(sprintf(
+				'Publisher must be an instance of Person or Organization. %s given',
+				get_class($publisher)
+			));
+		}
+	}
+
+	final public function setReviews(Review ...$reviews): self
+	{
+		foreach ($reviews as $review) {
+			$this->addReview($review);
+		}
+
+		return $this;
+	}
+
+	final public function addReview(Review $review): self
+	{
+		return $this->_add('review', $review);
+	}
+
+	final public function setSourceORganization(Organization $org): self
+	{
+		return $this->_set('sourceOrganization', $org);
+	}
+
+	final public function setSponsor(Thing $sponsor): self
+	{
+		if ($sponsor instanceof Person or $sponsor instanceof Organization) {
+			return $this->_set('sponsor', $sponsor);
+		} else {
+			throw new \InvalidArgumentException(sprintf(
+				'Sponsor must be an instance of Person or Organization. %s given',
+				get_class($sponsor)
+			));
+		}
+	}
+
+	final public function setText(String $text): self
+	{
+		return $this->_set('text', $text);
+	}
+
+	final public function setVideo(VideoObject $video): self
+	{
+		return $this->_set('video', $video);
+	}
+
+	final public function setWorkExample(CreativeWord $work): self
+	{
+		return $this->_set('workExample', $work);
+	}
 }
